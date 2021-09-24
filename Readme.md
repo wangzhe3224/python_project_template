4abc045cd90f62890fae5c4cce839b53d7e41932
Zhe Wang <wangzhetju@gmail.com>
2021-09-24 22:39:06 +0100

    fix flake8

--- Readme.md
+++ Readme.md
@@ -1,3 +1,6 @@
 # Python Start Project Template
 
+![Tests](https://github.com/wangzhe3224/python_project_template/actions/workflows/test.yml/badge.svg)
+
+
 : )
--- tradingbot/order.py
+++ tradingbot/order.py
@@ -5,13 +5,13 @@ class OrderState(enum.Enum):
     open = enum.auto()
     filled = enum.auto()
     cancelled = enum.auto()
-    
+
 
 def open_order(o: OrderState) -> OrderState:
     return {
         OrderState.filled: OrderState.filled,
         OrderState.open: OrderState.open,
-        OrderState.cancelled: OrderState.cancelled
+        OrderState.cancelled: OrderState.cancelled,
     }[o]
 
 
@@ -19,12 +19,13 @@ def fill(o: OrderState) -> OrderState:
     return {
         OrderState.filled: OrderState.filled,
         OrderState.open: OrderState.filled,
-        OrderState.cancelled: OrderState.cancelled
+        OrderState.cancelled: OrderState.cancelled,
     }[o]
-    
+
+
 def cancel(o: OrderState) -> OrderState:
     return {
         OrderState.filled: OrderState.filled,
         OrderState.open: OrderState.cancelled,
-        OrderState.cancelled: OrderState.cancelled
+        OrderState.cancelled: OrderState.cancelled,
     }[o]
